35f7d095b378dbca8d7b994758c01d5252db0c23
mohan-krishna003 <mohanulam093@gmail.com>
2025-04-13 15:32:59 +0530

    testing

--- SRC/sam.py
+++ SRC/sam.py
@@ -1,3 +1,5 @@
 print("Version 1")
 print("Version 12")
 print("ersionn 19")
+for i in range(10):
+    print(i)
--- SRC/sample.py
+++ SRC/sample.py
@@ -1,2 +1,4 @@
 print("Version 1")
 print("Version 13")
+for i in range(10):
+    print(i)
